af77bc97c58523caff4b1fa0a5eba55594c44b34
Dave Bulkin <dbulkin@hotmail.com>
2024-03-21 22:01:42 -0400

    animation clocked

--- main.ts
+++ main.ts
@@ -11,14 +11,13 @@ import GameBoard from "./gameboard.js"
 ///     settings
 ///     score?
 ///
-/// instancing?
 /// scoreboard update out of render
 /// game settings on web
 /// new game button
-/// clock for animation
 /// ability to save and load state
 /// high score in cookies
 /// minify
+/// undo/redo?
 
 interface coordinate {
   row: number;
@@ -43,46 +42,53 @@ async function main() {
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
     const [row, col] = renderer.getGridIndicesFromMouse(mouseX, mouseY);
-    gameBoard.hover({row,col})
+    gameBoard.hover({ row, col })
   }
   canvas.addEventListener("mousemove", handleMouseHover);
   canvas.addEventListener("mouseleave", gameBoard.mouseExit.bind(gameBoard));
 
-  const moverate = 0.2;
-  function animationLoop(){
+  const moverate = .1;
+  function animationLoop(startTime: number) {
+    const elapsedTime = performance.now() - startTime;
     renderer.renderBlocks();
     renderer.renderScoreBoard();
-    
-    // todo: add a clock
-    let turnItOff = true;
-    for(let r = 0; r < gameSettings.numRows; r++){
-      for(let c = 0; c < gameSettings.numColumns; c++){
-        gameBoard.offsetx[r][c] = Math.max(gameBoard.offsetx[r][c] - moverate, 0);
-        gameBoard.offsety[r][c] = Math.max(gameBoard.offsety[r][c] - moverate, 0);
-        if(gameBoard.offsetx[r][c]>0 || gameBoard.offsety[r][c]>0){
-          turnItOff=false;
+
+    const numSteps = Math.floor(elapsedTime * moverate);
+    // increment startTime based on how much has been "used"
+    startTime += numSteps / moverate;
+    let turnItOff = false;
+    for (let step = 0; step < numSteps; step++) {
+      turnItOff = true;
+      for (let r = 0; r < gameSettings.numRows; r++) {
+        for (let c = 0; c < gameSettings.numColumns; c++) {
+          gameBoard.offsetx[r][c] = Math.max(gameBoard.offsetx[r][c] - 0.1, 0);
+          gameBoard.offsety[r][c] = Math.max(gameBoard.offsety[r][c] - 0.1, 0);
+          if (gameBoard.offsetx[r][c] > 0 || gameBoard.offsety[r][c] > 0) {
+            turnItOff = false;
+          }
         }
       }
     }
-    if(turnItOff){
+    if (turnItOff) {
       gameBoard.doAnimation = false;
       gameBoard.blocksDirty = true;
     } else {
-      requestAnimationFrame(animationLoop);
+      requestAnimationFrame(() => animationLoop(startTime));
     }
   }
+
   function gameLoop() {
     gameBoard.update();
-    
+
     if (gameBoard.doAnimation) {
-      requestAnimationFrame(animationLoop);
+      requestAnimationFrame(() => animationLoop(performance.now()));
     }
-  
-    if(gameBoard.blocksDirty){
+
+    if (gameBoard.blocksDirty) {
       renderer.renderBlocks();
       renderer.renderPreview(gameBoard.blocksToPop);
       renderer.renderScoreBoard();
-      gameBoard.blocksDirty=false;
+      gameBoard.blocksDirty = false;
     }
 
     // Schedule the next iteration of the game loop
